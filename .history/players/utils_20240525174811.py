58e5e73ed5a59ff0219ab5e92499e1ead6b7f00d
nikos <nikospopulidis@gmail.com>
2024-05-26 08:47:10 +0300

    from 1 to 100

---
+++ .history/players/utils_20240525174811.py
@@ -0,0 +1,9 @@
+# realfootballsim/players/utils.py
+
+def generate_player_stats():
+    # Все характеристики равны 1
+    strength = 1
+    stamina = 1
+    pace = 1
+
+    return strength, stamina, pace
