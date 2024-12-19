ccbba4040a455b466a03a29ad54b0cb140be2ba4
nikos <nikospopulidis@gmail.com>
2024-12-19 20:18:24 +0300

    celery с новым движком

--- realfootballsim/celery.py
+++ realfootballsim/celery.py
@@ -1,6 +1,5 @@
 import os
 from celery import Celery
-from celery.schedules import crontab
 
 # Устанавливаем переменную окружения для настроек Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realfootballsim.settings')
@@ -9,20 +8,6 @@ app = Celery('realfootballsim')
 
 # Загружаем настройки из settings.py
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# Настраиваем периодические задачи
-app.conf.beat_schedule = {
-    # Симуляция матчей (каждую минуту)
-    'simulate-matches': {
-        'task': 'tournaments.check_and_simulate_matches',
-        'schedule': crontab(),  # каждую минуту
-    },
-    # Проверка окончания сезона (каждый день в полночь)
-    'check-season-end': {
-        'task': 'tournaments.check_season_end',
-        'schedule': crontab(minute=0, hour=0),  # в 00:00
-    }
-}
 
 # Автоматически обнаруживаем и регистрируем задачи из всех приложений Django
 app.autodiscover_tasks()
