b50a8041aa5bb06e3d42e76e81911111a9a95873
nikos <nikospopulidis@gmail.com>
2024-12-21 18:58:28 +0300

    все матчи можно просмотреть

--- matches/urls.py
+++ matches/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('create/', views.CreateMatchView.as_view(), name='create_match'),
     path('<int:pk>/', views.MatchDetailView.as_view(), name='match_detail'),
     path('<int:match_id>/simulate/', views.simulate_match_view, name='simulate_match'),
+    path('championship/<int:championship_id>/matches/', views.MatchListView.as_view(), name='championship_matches'),
 ]
