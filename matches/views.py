77327e543494fe347df879185f0ae848e03ce20e
nikos <nikospopulidis@gmail.com>
2024-12-21 18:58:28 +0300

    все матчи можно просмотреть

--- matches/views.py
+++ matches/views.py
@@ -1,30 +1,52 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import CreateView, DetailView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.utils import timezone
+from django.db.models import Q
 from .models import Match, MatchEvent
 from clubs.models import Club
-#from .match_simulation import simulate_match
 
 class CreateMatchView(CreateView):
     model = Match
-    fields = ['home_team', 'away_team', 'datetime']  # Исправлено с date на datetime
+    fields = ['home_team', 'away_team', 'datetime']
     template_name = 'matches/create_match.html'
 
     def form_valid(self, form):
         match = form.save()
         return redirect(reverse('matches:match_detail', kwargs={'pk': match.pk}))
 
-class MatchDetailView(DetailView):
+class MatchDetailView(LoginRequiredMixin, DetailView):
     model = Match
     template_name = 'matches/match_detail.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['match_events'] = MatchEvent.objects.filter(match=self.object).order_by('minute')
+        context['is_user_team'] = (
+            self.request.user.club == self.object.home_team or 
+            self.request.user.club == self.object.away_team
+        )
         return context
+
+class MatchListView(LoginRequiredMixin, ListView):
+    model = Match
+    template_name = 'matches/match_list.html'
+    context_object_name = 'matches'
+
+    def get_queryset(self):
+        championship_id = self.kwargs.get('championship_id')
+        if championship_id:
+            return Match.objects.filter(
+                championshipmatch__championship_id=championship_id
+            ).order_by('championshipmatch__round', 'datetime')
+        # Если нет championship_id, показываем матчи команды пользователя
+        return Match.objects.filter(
+            Q(home_team=self.request.user.club) | 
+            Q(away_team=self.request.user.club)
+        )
 
 @login_required
 def simulate_match_view(request, match_id):
@@ -41,7 +63,7 @@ def simulate_match_view(request, match_id):
         match = Match.objects.create(
             home_team=club,
             away_team=opponent,
-            datetime=timezone.now(),  # Исправлено с date на datetime
+            datetime=timezone.now(),
             status='scheduled'
         )
         match_id = match.id
@@ -58,13 +80,3 @@ def simulate_match_view(request, match_id):
         'match': match,
         'match_events': match_events,
     })
-
-@method_decorator(login_required, name='dispatch')
-class MatchListView(ListView):
-    model = Match
-    template_name = 'matches/match_list.html'
-    context_object_name = 'matches'
-
-    def get_queryset(self):
-        return Match.objects.filter(home_team=self.request.user.club) | \
-               Match.objects.filter(away_team=self.request.user.club)
