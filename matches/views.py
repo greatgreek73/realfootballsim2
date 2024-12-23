6e0b0a819f283ebd07ade5e474b1be3aca060a90
nikos <nikospopulidis@gmail.com>
2024-12-23 21:38:25 +0300

    дважды тиковая генерация

--- matches/views.py
+++ matches/views.py
@@ -3,12 +3,17 @@ from django.views.generic import CreateView, DetailView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseNotAllowed
 from django.utils.decorators import method_decorator
 from django.utils import timezone
 from django.db.models import Q
+
 from .models import Match, MatchEvent
 from clubs.models import Club
+
+# Импорт Celery-задач
+from .tasks import simulate_match_minute, broadcast_minute_events_in_chunks
+
 
 class CreateMatchView(CreateView):
     model = Match
@@ -19,18 +24,22 @@ class CreateMatchView(CreateView):
         match = form.save()
         return redirect(reverse('matches:match_detail', kwargs={'pk': match.pk}))
 
+
 class MatchDetailView(LoginRequiredMixin, DetailView):
     model = Match
     template_name = 'matches/match_detail.html'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['match_events'] = self.object.events.order_by('minute')
         context['is_user_team'] = (
-            self.request.user.club == self.object.home_team or 
-            self.request.user.club == self.object.away_team
+            self.request.user.is_authenticated and (
+                self.request.user.club == self.object.home_team or 
+                self.request.user.club == self.object.away_team
+            )
         )
         return context
+
 
 class MatchListView(LoginRequiredMixin, ListView):
     model = Match
@@ -49,6 +58,7 @@ class MatchListView(LoginRequiredMixin, ListView):
             Q(away_team=self.request.user.club)
         )
 
+
 @login_required
 def get_match_events(request, match_id):
     match = get_object_or_404(Match, id=match_id)
@@ -65,9 +75,13 @@ def get_match_events(request, match_id):
         }
     })
 
+
 @login_required
 def simulate_match_view(request, match_id):
-    # Если match_id = 0, создаем новый тестовый матч
+    """
+    Создаёт тестовый матч, если match_id=0.
+    Иначе просто редиректит на страницу матча.
+    """
     if match_id == 0:
         # Получаем клуб пользователя
         club = request.user.club
@@ -88,3 +102,34 @@ def simulate_match_view(request, match_id):
     
     # Редирект на страницу матча
     return redirect('matches:match_detail', pk=match_id)
+
+
+@login_required
+def simulate_match_minute_view(request, match_id):
+    """
+    Пример ручного вызова симуляции ОДНОЙ виртуальной минуты:
+      1) Запустить задачу Celery на simulate_match_minute
+      2) Запустить задачу Celery на "chunked" broadcast (за 10 секунд) 
+         событий этой минуты
+    """
+    if request.method != 'POST':
+        return HttpResponseNotAllowed(['POST'], "This endpoint requires POST request")
+
+    match = get_object_or_404(Match, id=match_id)
+
+    # Запускаем задачу симуляции одной минуты
+    simulate_match_minute.delay(match_id)
+
+    # В этот момент match.current_minute ещё старый (до симуляции), 
+    # но мы предполагаем, что за "текущую" минуту (match.current_minute+1)
+    # будут созданы события.
+    # Для упрощения считаем, что Celery всё сделает асинхронно.
+    next_minute = match.current_minute + 1
+
+    # Запускаем задачу "пошаговой" рассылки событий этой минуты
+    broadcast_minute_events_in_chunks.delay(match_id, next_minute, duration=10)
+
+    return JsonResponse({
+        'status': 'ok',
+        'message': f"Requested simulation of 1 minute for match {match_id} + chunked broadcast."
+    })
