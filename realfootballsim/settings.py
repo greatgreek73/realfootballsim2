5f038ab232f6a584b72d326024a84f0a0c10acc5
greatgreek73 <33379483+greatgreek73@users.noreply.github.com>
2025-05-19 18:09:25 +0400

    Centralize tick duration in settings

--- realfootballsim/settings.py
+++ realfootballsim/settings.py
@@ -7,6 +7,10 @@ IS_PRODUCTION = os.environ.get('IS_PRODUCTION')  # If not set, will be None
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Global duration of one simulated minute (seconds).
+# Can be overridden via the MATCH_TICK_SECONDS environment variable.
+MATCH_TICK_SECONDS = int(os.environ.get('MATCH_TICK_SECONDS', 5))
+
 # Default dev settings
 SECRET_KEY = 'django-insecure-0p3aqax2r2xolyvtfda6q_aa@q1l6n!w4$8sjo1ed&*)h*2l37'
 DEBUG = True
