41475806852765735f51314d8f363507f74b6644
nikos <nikospopulidis@gmail.com>
2025-11-09 10:14:57 +0300

    новый шаблон для страниц-2

--- frontend/src/menu-items.backup.ts
+++ frontend/src/menu-items.backup.ts
@@ -15,22 +15,6 @@ export const leftMenuItems: MenuItem[] = [
     color: "text-primary",
     href: "/my-club",
   },
-  {
-    id: "single-menu",
-    icon: "NiDocumentFull",
-    label: "menu-single-menu",
-    color: "text-primary",
-    href: "/single-menu",
-  },
-
-    {
-    id: "external-link",
-    icon: "NiArrowUpRightSquare",
-    label: "menu-external-link",
-    color: "text-primary",
-    href: "https://themeforest.net/",
-    isExternalLink: true,
-  },
 ];
 
 export const leftMenuBottomItems: MenuItem[] = [
--- frontend/src/menu-items.ts
+++ frontend/src/menu-items.ts
@@ -54,22 +54,6 @@ export const leftMenuItems: MenuItem[] = [
       { id: "transfers-history", label: "History", href: "/transfers/history" },
     ],
   },
-  {
-    id: "single-menu",
-    icon: "NiDocumentFull",
-    label: "menu-single-menu",
-    color: "text-primary",
-    href: "/single-menu",
-  },
-
-  {
-    id: "external-link",
-    icon: "NiArrowUpRightSquare",
-    label: "menu-external-link",
-    color: "text-primary",
-    href: "https://themeforest.net/",
-    isExternalLink: true,
-  },
 ];
 
 export const leftMenuBottomItems: MenuItem[] = [
